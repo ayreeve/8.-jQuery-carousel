eac2c3d289476cce610e50ce0fec2194204d59e1
Kaja <ayreeve@wp.pl>
2017-05-26 17:49:04 +0200

    indicators added

--- js/scripts.js
+++ js/scripts.js
@@ -19,13 +19,35 @@ $(document).ready(function () {
         });
     }
 
+    function moveLastSlide() {
+        var $firstItem = $carouselList.find('li:first'),
+            $lastItem = $carouselList.find('li:last');
+        $firstItem.before($lastItem);
+        $carouselList.css({
+            marginRight: +600
+        });
+    }
+
+    $('#js-left-button').click(function () {
+        moveLastSlide();
+        $carouselList.animate({
+            'marginLeft': 0
+        }, 800);
+    });
+
     $('#js-right-button').click(function () {
         changeSlides();
     });
 
-    $('#js-left-button').click(function () {
-        $carouselList.animate({
-            'marginLeft': 600
-        }, 800);
+    var $indicators = $('.indicators ion-ios-circle-outline');
+    $indicators.each(function (index, element) {
+
+        $('$indicators').click(function () {
+            $(this).removeClass('active');
+            $(this).addClass('active');
+            changeSlides(index);
+        });
+
     });
+
 });
